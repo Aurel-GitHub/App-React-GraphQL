020d3f1fd32560ab12a725cdfa14d56bfee96ca0
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-10-25 15:43:21 +0200

    end exo

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+function App() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <div className="App">
+      <h1>SpaceX App</h1>
+      {data.launches.map((launch) => (
+        <li>{launch.launch_date_utc}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
